3fe30784fef8eb51b8ba23985451dc9bf91d6d66
dmtarasov <dmtarasov@ptsecurity.com>
2024-04-23 17:08:12 +0300

    Add parse method in parallel

--- main.go
+++ main.go
@@ -29,7 +29,6 @@ func NewCityTemp(value float64) CityTemp {
 		return 0
 	}
 */
-
 func NewCities() Cities {
 	return make(Cities)
 }
@@ -79,6 +78,69 @@ func ParseFileV1(file *os.File) Cities {
 	return cities
 }
 
+func ParseFileV2(file *os.File) Cities {
+	defer measureExecTime(time.Now())
+
+	cities := NewCities()
+
+	producer := func() <-chan []string {
+		result := make(chan []string, 0xFF)
+
+		go func() {
+			defer close(result)
+
+			localCopy := make([]string, 0xFF)
+			var count int
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				localCopy[count] = scanner.Text()
+				count++
+				if count > 0xFF-1 {
+					result <- localCopy
+					count = 0
+				}
+			}
+			if count != 0 {
+				result <- localCopy[:count]
+			}
+
+		}()
+		return result
+	}
+
+	result := producer()
+
+	for item := range result {
+		for _, text := range item {
+			index := strings.Index(text, ";")
+			if index == -1 {
+				continue
+			}
+
+			city := text[:index]
+			temp, err := strconv.ParseFloat(text[index+1:], 64)
+			if err != nil {
+				continue
+			}
+
+			if value, ok := cities[city]; ok {
+				value.sum += temp
+				value.count += 1
+				if value.min > temp {
+					value.min = temp
+				}
+				if value.max < temp {
+					value.max = temp
+				}
+				cities[city] = value
+			} else {
+				cities[city] = NewCityTemp(temp)
+			}
+		}
+	}
+	return cities
+}
+
 func main() {
 	file, err := os.Open("weather_stations.csv")
 	if err != nil {
@@ -86,11 +148,12 @@ func main() {
 	}
 	defer file.Close()
 
-	_ = ParseFileV1(file)
-
-	//	for k, v := range cities {
-	//		if v.count > 1 {
-	//			fmt.Printf("City:%s, min=%f/max=%f/avg=%f\n", k, v.min, v.max, v.calculateAvg())
-	//		}
-	//	}
+	_ = ParseFileV2(file)
+	/*
+		for k, v := range cities {
+			if v.count > 1 {
+				fmt.Printf("City:%s, min=%f/max=%f/avg=%f\n", k, v.min, v.max, v.calculateAvg())
+			}
+		}
+	*/
 }
